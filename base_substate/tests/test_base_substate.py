OCA/server-ux

[FIX] base_substate: send email template on substate

base_substate/tests/test_base_substate.py
@@ -17,6 +17,14 @@ class TestBaseSubstate(common.TransactionCase):
         cls.substate_test_sale = cls.env["base.substate.test.sale"]
         cls.substate_test_sale_line = cls.env["base.substate.test.sale.line"]
 
+        cls.mail_template = cls.env["mail.template"].create(
+            {
+                "name": "Waiting for legal documents",
+                "model_id": cls.substate_test_sale.id,
+                "subject": "Test Email Substate",
+            }
+        )
+
         cls.base_substate = cls.env["base.substate.mixin"]
         cls.substate_type = cls.env["base.substate.type"]
 
@@ -68,6 +76,7 @@ class TestBaseSubstate(common.TransactionCase):
                 "name": "Waiting for legal documents",
                 "sequence": 2,
                 "target_state_value_id": cls.substate_val_sale.id,
+                "mail_template_id": cls.mail_template.id,
             }
         )
 
@@ -106,12 +115,17 @@ class TestBaseSubstate(common.TransactionCase):
         )
         self.assertTrue(so_test1.state == "draft")
         self.assertTrue(so_test1.substate_id == self.substate_under_negotiation)
-
+        self.assertNotIn(
+            self.mail_template.subject, so_test1.message_ids.mapped("subject")
+        )
         # Test that validation of sale order change substate_id
         so_test1.button_confirm()
         self.assertTrue(so_test1.state == "sale")
         self.assertTrue(so_test1.substate_id == self.substate_wait_docs)
-
+        # Check some message_ids are created and sent email
+        self.assertIn(
+            self.mail_template.subject, so_test1.message_ids.mapped("subject")
+        )
         # Test that substate_id is set to false if
         # there is not substate corresponding to state
         so_test1.button_cancel()

OCA/tier-validation

base_substate/tests/test_base_substate.py
@@ -14,17 +14,14 @@ class TestBaseSubstate(common.TransactionCase):
         SaleTest._test_setup_models(cls.env, [SaleTest, LineTest])
         LineTest._test_setup_model(cls.env)
 
-        cls.substate_test_sale = cls.env["base.substate.test.sale"]
-        cls.substate_test_sale_line = cls.env["base.substate.test.sale.line"]
-
-        cls.base_substate = cls.env["base.substate.mixin"]
-        cls.substate_type = cls.env["base.substate.type"]
-
-        cls.substate_type._fields["model"].selection.append(
-            ("base.substate.test.sale", "Sale Order")
+        cls.mail_template = cls.env["mail.template"].create(
+            {
+                "name": "Waiting for legal documents",
+                "model_id": cls.sale_test_model.id,
+                "subject": "Test Email Substate",
+            }
         )
-
-        cls.substate_type = cls.env["base.substate.type"].create(
+        cls.sale_test_substate_type = cls.substate_type.create(
             {
                 "name": "Sale",
                 "model": "base.substate.test.sale",
@@ -68,6 +65,7 @@ class TestBaseSubstate(common.TransactionCase):
                 "name": "Waiting for legal documents",
                 "sequence": 2,
                 "target_state_value_id": cls.substate_val_sale.id,
+                "mail_template_id": cls.mail_template.id,
             }
         )
 
@@ -106,12 +104,17 @@ class TestBaseSubstate(common.TransactionCase):
         )
         self.assertTrue(so_test1.state == "draft")
         self.assertTrue(so_test1.substate_id == self.substate_under_negotiation)
-
+        self.assertNotIn(
+            self.mail_template.subject, so_test1.message_ids.mapped("subject")
+        )
         # Test that validation of sale order change substate_id
         so_test1.button_confirm()
         self.assertTrue(so_test1.state == "sale")
         self.assertTrue(so_test1.substate_id == self.substate_wait_docs)
-
+        # Check some message_ids are created and sent email
+        self.assertIn(
+            self.mail_template.subject, so_test1.message_ids.mapped("subject")
+        )
         # Test that substate_id is set to false if
         # there is not substate corresponding to state
         so_test1.button_cancel()