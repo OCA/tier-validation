Tecnativa/server-ux

[ADD] base_search_custom_field_filter: Add custom filters for fields via UI
This module allows to define custom filters in the search views for an specific
field belonging to the document or any other related document.

This nature makes the definition quite technical, but once done, it adds the
element in the UI for regular user use.

Configuration
=============

* Go to *Settings > Technical > User Interface > Custom Field Filters*.
* Create a new record, and define following information:

  * The **Model** for which you are defining the filter. It will appear in all
    the search views of this model.
  * The label you want to see on the search line on the **Name** field. This
    field allows translations for proper UI in different languages.
  * The **Expression**, which is the field chain string with dot notation.
    Examples: `product_id`, `product_id.seller_ids.name`, `partner_id.lang`.
  * Optionally, you can fill **Position After** for indicating after which
    existing field (technical name) the filter will appear. If empty or not
    found, the filter will be added at the end.
* You can reorder records for determining sorting for multiple filters for the
  same model with the arrow handle in the left part.

Usage
=====

* Go to the menu entry for which you have defined the custom field filter.
* On the search bar, type anything.
* In the filter list, you will see the line for the element you have defined.

As demo data, a custom field filter is included for sample purposes.

Steps for trying this sample:

* Install `contacts` module.
* Go to *Contacts*.
* Type "english" and you'll find the filter "Language" at the end.

base_search_custom_field_filter/models/ir_ui_custom_field_filter.py
@@ -1,0 +1,42 @@
+# Copyright 2020 Tecnativa - Carlos Dauden
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, exceptions, fields, models
+
+
+class IrUiCustomFilter(models.Model):
+    _name = "ir.ui.custom.field.filter"
+    _description = "Custom UI field filter"
+    _order = "sequence, model_id, id"
+
+    sequence = fields.Integer()
+    model_id = fields.Many2one(comodel_name="ir.model", required=True)
+    model_name = fields.Char(
+        related="model_id.model", store=True, readonly=True, index=True,
+        string="Model name",
+    )
+    name = fields.Char(required=True, translate=True)
+    expression = fields.Char(required=True)
+    position_after = fields.Char(
+        help="Optional field name for putting the filter after that one. "
+             "If empty or not found, it will be put at the end.",
+    )
+
+    def _get_related_field(self):
+        """Determine the chain of fields."""
+        self.ensure_one()
+        related = self.expression.split('.')
+        target = self.env[self.model_name]
+        for name in related:
+            field = target._fields[name]
+            target = target[name]
+        return field
+
+    @api.constrains('model_id', 'expression')
+    def _check_expression(self):
+        for record in self:
+            try:
+                record._get_related_field()
+            except KeyError:
+                raise exceptions.ValidationError(_("Incorrect expression."))

OCA/tier-validation

base_search_custom_field_filter/models/ir_ui_custom_field_filter.py
@@ -1,0 +1,42 @@
+# Copyright 2020 Tecnativa - Carlos Dauden
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, exceptions, fields, models
+
+
+class IrUiCustomFilter(models.Model):
+    _name = "ir.ui.custom.field.filter"
+    _description = "Custom UI field filter"
+    _order = "model_id, sequence, id"
+
+    sequence = fields.Integer()
+    model_id = fields.Many2one(comodel_name="ir.model", required=True)
+    model_name = fields.Char(
+        related="model_id.model", store=True, readonly=True, index=True,
+        string="Model name",
+    )
+    name = fields.Char(required=True, translate=True)
+    expression = fields.Char(required=True)
+    position_after = fields.Char(
+        help="Optional field name for putting the filter after that one. "
+             "If empty or not found, it will be put at the end.",
+    )
+
+    def _get_related_field(self):
+        """Determine the chain of fields."""
+        self.ensure_one()
+        related = self.expression.split('.')
+        target = self.env[self.model_name]
+        for name in related:
+            field = target._fields[name]
+            target = target[name]
+        return field
+
+    @api.constrains('model_id', 'expression')
+    def _check_expression(self):
+        for record in self:
+            try:
+                record._get_related_field()
+            except KeyError:
+                raise exceptions.ValidationError(_("Incorrect expression."))